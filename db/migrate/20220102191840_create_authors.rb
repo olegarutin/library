5df2884464d1510871d3425adfed63af535a5634
oleg <o1eg.34ac@gmail.com>
2022-01-03 23:36:00 +0200

    Create author table

--- db/migrate/20220102191840_create_authors.rb
+++ db/migrate/20220102191840_create_authors.rb
@@ -1,6 +1,10 @@
 class CreateAuthors < ActiveRecord::Migration[6.1]
   def change
     create_table :authors do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :gender
+      t.date :birthday
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_31_182405) do
+ActiveRecord::Schema.define(version: 2022_01_02_191840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "gender"
+    t.date "birthday"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
