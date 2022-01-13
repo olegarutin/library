7bb805242150b33016709b30db2ca13f10b1747d
oleg <o1eg.34ac@gmail.com>
2022-01-13 22:16:16 +0200

    Add associating Comments with Posts and Authors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :author
+  has_many :comments, dependent: :destroy
   validates :title, length: { in: 1..40 }
   validates :image, allow_blank: true, format: { with: %r{.(gif|jpg|png)\Z}i, message: 'must be a URL for GIF, JPG or PNG image.' }
   validates :content, presence: true
