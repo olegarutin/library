e819ca9631b77f2236365e4ef0ef4900a0b367a4
oleg <o1eg.34ac@gmail.com>
2022-01-18 22:42:38 +0200

    Fix image valide

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,6 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   validates :title, length: { in: 1..40 }
-  validates :image, allow_blank: true, format: { with: %r{(http:\/\/|https:\/\/).(gif|jpg|png)\Z}i, message: INVALIDED_IMAGE_ERROR }
+  validates :image, allow_blank: false, format: { with: %r{.(gif|jpg|png)\Z}i, message: INVALIDED_IMAGE_ERROR }
   validates :content, presence: true
 end
