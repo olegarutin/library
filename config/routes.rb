ac965af5cbb9e29f1dfbdbdba1b7a3a7379c2033
oleg <o1eg.34ac@gmail.com>
2022-01-21 22:30:29 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root 'posts#index'
 
+  get 'signup', to: 'authors#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :sessions
+  resources :authors
+
   resources :posts do
     resources :comments
   end
