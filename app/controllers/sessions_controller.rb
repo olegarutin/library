843a88408787238f8a25d342164b9254b452f286
oleg <o1eg.34ac@gmail.com>
2022-01-22 23:21:43 +0200

    Update application controller and delete white spaces

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,14 @@ class SessionsController < ApplicationController
     author = Author.find_by_email(params[:email])
     if author && author.authenticate(params[:password])
       session[:author_id] = author.id
-      redirect_to root_url, notice: "Logged in!"
+      redirect_to root_url, notice: 'Logged in!'
     else
-      flash.now.alert = "Email or password is invalid."
+      flash.now.alert = 'Email or password is invalid.'
     end
   end
 
   def destroy
     session[:author_id] = nil
-    redirect_to root_url, notice: "Logged out!"
+    redirect_to root_url, notice: 'Logged out!'
   end
 end
