1a7a90d630c35ad098f07beacb1d5459503e6a68
oleg <o1eg.34ac@gmail.com>
2022-01-23 23:05:52 +0200

    Add password column to author table

--- db/migrate/20220121185836_add_authorization_to_authors.rb
+++ db/migrate/20220121185836_add_authorization_to_authors.rb
@@ -1,6 +1,8 @@
 class AddAuthorizationToAuthors < ActiveRecord::Migration[6.1]
   def change
     add_column :authors, :email, :string
+    add_column :authors, :password, :string
+    add_column :authors, :password_confirmation, :string
     add_column :authors, :password_digest, :string
   end
 end
