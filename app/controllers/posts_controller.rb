152c39d5a7e5edfee1b9097f6f73df1aee1f153a
oleg <o1eg.34ac@gmail.com>
2022-01-23 23:08:44 +0200

    Add method protect_editing and remove comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,21 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
+  before_action :authorize, except: %i[show index]
+  before_action :protect_editing, only: %i[update edit destroy]
 
-  # GET /posts or /posts.json
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1 or /posts/1.json
   def show
     @comment = @post.comments.build
     @post.increment!(:count_views)
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
-  def edit
-  end
-
-  # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
 
@@ -36,7 +30,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1 or /posts/1.json
   def update
     respond_to do |format|
       if @post.update(post_params)
@@ -49,7 +42,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1 or /posts/1.json
   def destroy
     @post.destroy
 
@@ -61,13 +53,18 @@ class PostsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_post
     @post = Post.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def post_params
     params.require(:post).permit(:title, :content, :image, :author_id)
+  end
+
+  def protect_editing
+    return if @post.nil? || @post.author_id == current_user.id
+
+    flash[:notice] = 'Access denied as you are not owner of this Post'
+    redirect_to root_path
   end
 end
