f1a591938131dc753e7510e1a65546799d56d750
oleg <o1eg.34ac@gmail.com>
2022-01-24 21:57:19 +0200

    Fix mistake

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  private
-
   def current_user
     @current_user = Author.find(session[:author_id]) if session[:author_id]
   end
+
+  private
 
   def authorize
     redirect_to login_url, alert: 'Not authorized' if current_user.nil?
--- app/javascript/packs/toggle.js
+++ app/javascript/packs/toggle.js
@@ -1,6 +1,6 @@
 window.toggle = function() {
   let selectBox = document.querySelector(".js-toggle"),
-      url = selectBox.dataset.path +'?published=' + Boolean(Number(selectBox.value));
+      url = selectBox.dataset.path +'?published=' + !Boolean(Number(selectBox.value));
 
   window.location.href = url;
 };
