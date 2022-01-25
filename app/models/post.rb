e7a571bf8bf0de1b713e26721daf95df26c82fca
oleg <o1eg.34ac@gmail.com>
2022-01-25 17:49:12 +0200

    Add validation to post image

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,12 @@
 class Post < ApplicationRecord
+  has_one_attached :image
+
   INVALIDED_IMAGE_ERROR = 'must be a URL for GIF, JPG or PNG image.'.freeze
 
   belongs_to :author
   has_many :comments, dependent: :destroy
 
   validates :title, length: { in: 1..40 }
-  validates :image, allow_blank: false, format: { with: %r{.(gif|jpg|png)\Z}i, message: INVALIDED_IMAGE_ERROR }
+  validates :image, presence: true
   validates :content, presence: true
 end
