2e18b7a1a079b6a930190cafcf20eeaec5b91f69
oleg <o1eg.34ac@gmail.com>
2022-01-25 16:00:48 +0200

    Create update action and method check_editable

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,6 @@
 class AuthorsController < ApplicationController
+  before_action :set_author, :check_editable, only: %i[update edit]
+
   def new
     @author = Author.new
   end
@@ -13,9 +15,31 @@ class AuthorsController < ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+      if @author.update(author_params)
+        format.html { redirect_to author_path, notice: 'Author was successfully updated.' }
+        format.json { render :show, status: :ok, location: @author }
+      else
+        format.html { render 'author/edit', status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
+  def set_author
+    @author = Author.find(params[:id])
+  end
+
   def author_params
-    params.require(:author).permit(:email, :first_name, :last_name, :password, :password_confirmation)
+    params.require(:author).permit(:email, :first_name, :birthday, :gender, :last_name, :password, :password_confirmation, :profile_image)
+  end
+
+  def check_editable
+    return if @author.nil? || @author.id == current_user.id
+
+    flash[:notice] = 'Access denied because you are not the author'
+    redirect_to root_path
   end
 end
