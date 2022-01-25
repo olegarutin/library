133c47289d5f5a7227a6dbec5d6deb46f5e2af78
oleg <o1eg.34ac@gmail.com>
2022-01-25 15:56:09 +0200

    Remove seeds

--- db/seeds.rb
+++
@@ -1,6 +0,0 @@
-5.times do
-  Author.create!(first_name: Faker::Name.unique.first_name,
-                last_name: Faker::Name.unique.last_name,
-                gender: Faker::Gender.binary_type,
-                birthday: Faker::Date.unique.birthday)
-end
