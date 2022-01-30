782bda2186b8fc54e624b3008a3384a928840018
oleg <o1eg.34ac@gmail.com>
2022-01-30 23:36:32 +0200

    Add new route

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,14 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   get 'search', to: 'posts#search'
-
+  get 'vote', to: 'likes#vote'
   resources :comment_statuses, only: %i[update]
   resources :sessions
   resources :authors
 
   resources :posts do
-    resources :comments
+    resources :comments do
+      resources :likes
+    end
   end
 end
