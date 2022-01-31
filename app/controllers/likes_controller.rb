efe774105765a7f2150b0598c69e4e3bee300ea4
oleg <o1eg.34ac@gmail.com>
2022-01-31 23:11:33 +0200

    Create controller Likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,31 @@
 class LikesController < ApplicationController
+  before_action :set_like, :find_comment
+
   def vote
-    redirect_to root_path
-    binding.pry
+    if already_voted?
+      @like.update(like_params)
+    else
+      @like = Like.create(like_params)
+    end
+    render 'like.js.erb'
+  end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:author_id, :comment_id, :vote_type)
+  end
+
+  def find_comment
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:like][:comment_id])
+  end
+
+  def set_like
+    @like = Like.find_by(author_id: current_user.id, comment_id: params[:like][:comment_id])
+  end
+
+  def already_voted?
+    Like.where(author_id: current_user.id, comment_id: params[:like][:comment_id]).exists?
   end
 end
