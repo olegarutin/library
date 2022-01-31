64930c91889926ee08ed1362ac8d78ff8e8d1485
oleg <o1eg.34ac@gmail.com>
2022-01-31 23:11:17 +0200

    Remove validation

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,8 +2,5 @@ class Like < ApplicationRecord
   belongs_to :author
   belongs_to :comment
 
-  validates :author, uniqueness: { scope: :author }
-  validates :comment, uniqueness: { scope: :comment }
-
   enum vote_type: %i[likes dislikes]
 end
