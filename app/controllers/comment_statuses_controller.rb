00e9c9a392898c5811d3b6f580e6cdccbebd84d5
oleg <o1eg.34ac@gmail.com>
2022-02-02 23:13:12 +0200

    Use ajax to select menu

--- app/controllers/comment_statuses_controller.rb
+++ app/controllers/comment_statuses_controller.rb
@@ -1,15 +1,25 @@
 class CommentStatusesController < ApplicationController
   before_action :set_comments, only: %i[update]
+  before_action :set_post
 
   def update
     @comment.published!
     render 'comments/comment.js.erb'
   end
 
+  def status
+    @status = params[:status]
+    @selected_comments = @post.comments.where(status: @status)
+    render 'comments/comment.js.erb'
+  end
+
   private
 
   def set_comments
-    @post = Post.find(params[:id])
     @comment = Comment.find(params[:format])
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
