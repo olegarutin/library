740f95c4e800628b321c8c3b03f3a376587da0e5
oleg <o1eg.34ac@gmail.com>
2022-02-03 23:45:21 +0200

    Remove parent_id from comments table

--- db/migrate/20220203195752_add_ancestry_to_comments.rb
+++ db/migrate/20220203195752_add_ancestry_to_comments.rb
@@ -1,6 +1,5 @@
 class AddAncestryToComments < ActiveRecord::Migration[6.1]
   def change
-    add_column :comments, :parent_id, :integer
     add_column :comments, :ancestry, :string
     add_index :comments, :ancestry
   end
