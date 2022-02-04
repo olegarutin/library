4b0ace691aa6acc813f91089802ed9479197ec08
oleg <o1eg.34ac@gmail.com>
2022-02-04 18:55:18 +0200

    Remove useless file

--- app/javascript/packs/toggle.js
+++
@@ -1,6 +0,0 @@
-window.toggle = function() {
-  let selectBox = document.querySelector(".js-toggle"),
-      url = selectBox.dataset.path +'?published=' + !Boolean(Number(selectBox.value));
-
-  window.location.href = url;
-};
